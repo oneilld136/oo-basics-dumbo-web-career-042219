cb27f36eb96e750149e03f3bdd1cc088f3863a61
oneilld136 <oneilld136@gmail.com>
2019-04-24 16:05:30 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,45 @@
-# Make your shoe class here!
+class Shoe
+
+  attr_accessor :color,  :material,  :condition
+  attr_reader :brand
+  
+  def initialize(brand)
+    @brand= brand
+  end
+
+  def brand
+    @brand
+  end
+
+  def color=(color) #setter
+    @color = color
+  end
+
+  def color
+    @color
+  end
+
+  def material=(material)
+    @material=material
+  end
+
+  def material
+    @material
+  end
+
+  def genre=(genre)
+    @genre=genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def condition=(condition)
+    @condition=condition
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+  end
+end
